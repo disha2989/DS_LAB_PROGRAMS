654db4a16c849655772bbd95019f2500638bce88
disha2989 <71320045+disha2989@users.noreply.github.com>
2020-09-25 11:34:00 +0530

    Update stackprog.c

--- stackprog.c
+++ stackprog.c
@@ -39,8 +39,8 @@ if(stempty())
 printf("stack is empty\n");
 else
 {
-  for(i=st.top;i>=0;i--)
-  printf("%d\n",st.s[i]);
+for(i=st.top;i>=0;i--)
+printf("%d\n",st.s[i]);
 }
 }
 
@@ -51,37 +51,74 @@ st.top=-1;
 printf("IMPLEMENTATION OF THE STACK\n");
 
 do
-  {
-   printf("Main Menu\n");
-   printf("1.Push\t2.Pop\t3.Display\t4.exit\n");
-   printf("Enter your choice\n");
-   scanf("%d",&choice);
-   switch(choice)
-   {
-    case 1: printf("enter the item to be pushed\n");
-            scanf("%d",&item);
-            if(stfull())
-               printf("stack is full\n");
-            else
-                push(item);
-                break;
-    case 2: if(stempty())
-            printf("empty stack!underflow\n");
-            else
-               {
-                 item=pop();
-                 printf("the popped element is %d\n",item);
-               }
-                 break;
-    case 3: display();
-            break;
-    case 4: exit(0);
+{
+ printf("Main Menu\n");
+ printf("1.Push\t2.Pop\t3.Display\t4.exit\n");
+ printf("Enter your choice\n");
+ scanf("%d",&choice);
+ switch(choice)
+ {
+  case 1: printf("enter the item to be pushed\n");
+          scanf("%d",&item);
+          if(stfull())
+            {
+             printf("stack is full\n");
+
+           }
+          else
+
+              push(item);
+              break;
+  case 2: if(stempty())
+          { 
+          printf("empty stack!underflow\n");
+
+           }
+          else
+             {
+               item=pop();
+               printf("the popped element is %d\n",item);
+             }
+               break;
+  case 3: display();
+          break;
+  case 4: exit(0);
 }
 printf("do you want to continue?(if yes enter 1)\n");
 scanf("%d",&cont);
 }while(cont==1);
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
